987ff8487167d38a6ac435984e075302d407d9a5
Huong Lan To <tohuonglan1705@gmail.com>
2025-04-26 01:44:12 -0500

    Update Dockerfile

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = parseInt(process.env.PORT || '5000', 10);
+const port = parseInt(process.env.PORT || '8080', 10);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
